94bb058bd142dec38e0c4fcf6ab6e2138e0192cd
antonellavaleriagarcia <antonellavaleriagarcia@gmail.com>
2025-04-30 11:53:31 -0300

    finalizo validacion formulario registro

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,70 @@
 import React from 'react'
 import RegisterForm from '../components/Form/RegisterForm'
 import ClearisNavbar from '../components/ClearisNavbar/ClearisNavbar';
+import ValidationsRegister from '../components/Validations/ValidationsRegister';
+import { useRef, useState } from 'react';
 
 const Register = () => {
+
+    const emailRef = useRef(null);
+    const nameRef = useRef(null);
+    const lastnameRef = useRef(null);
+    const telRef = useRef(null);
+    const addressRef = useRef(null);
+    const passwordRef = useRef(null);
+    const repPasswordRef = useRef(null);
+    const [errores,setErrores] = useState({});
+    const [exito,setExito] = useState(false);
+
+  const manejarEnvio = (FormData) => {
+    const errores = ValidationsRegister( { datos: FormData });
+  
+
+    if (Object.keys(errores).length > 0) {
+      if(errores.email && emailRef.current){
+        emailRef.current.focus();
+      } else if(errores.name && nameRef.current){
+        nameRef.current.focus();
+      } else if(errores.lastname && lastnameRef.current){
+        lastnameRef.current.focus();
+      } else if(errores.tel && telRef.current){
+        telRef.current.focus();
+      } else if(errores.address && addressRef.current){
+        addressRef.current.focus();
+      } else if (errores.password && passwordRef.current){
+        passwordRef.current.focus();
+      } else if (errores.repPassword && repPasswordRef.current){
+        repPasswordRef.current.focus();
+      }
+      setErrores(errores);
+      setExito(false);
+    } else {
+      setErrores({});
+      setExito(true);
+      setTimeout(() => navigate("/registro"), 2000); 
+    };
+  };
+
+
+
     return (
         <div>
-            <ClearisNavbar />
-            <RegisterForm />
-        </div>
+        <ClearisNavbar />
+         {exito && (
+          <div className="login-success-message">
+            ¡Ingreso exitoso!
+          </div>
+         )}
+         {!exito && (
+          <div className="login-container">
+             <RegisterForm
+               onSubmit={manejarEnvio}
+              errores={errores}
+               refs={{ email: emailRef, name: nameRef, lastname: lastnameRef, tel: telRef, address: addressRef, password: passwordRef, repPassword: repPasswordRef }}
+            />
+          </div>
+         )}
+       </div>
     );
 };
 
