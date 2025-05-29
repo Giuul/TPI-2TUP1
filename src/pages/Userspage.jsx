5e646e15a56916982aedb7320e938227884e7fa6
antonellavaleriagarcia <antonellavaleriagarcia@gmail.com>
2025-05-29 13:14:01 -0300

    Creacion de component users y page users

---
+++ src/pages/Userspage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./pages.css";
+import ClearisFooter from '../components/ClearisFooter/ClearisFooter';
+import Users from '../components/Users/Users';
+
+const Userspage = () => {
+    return (
+        <div>
+            <Users />
+            <ClearisFooter />
+        </div>
+    )
+}
+
+export default Userspage
