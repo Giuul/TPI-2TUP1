a1d5cc8fbf24dad10f0429ea44341cee6fca01d5
valenggar <valentinagarcia11230@gmail.com>
2025-10-14 20:24:15 -0300

    edite un modal en turnos

--- src/components/Turnos/ModalPortal.jsx
+++ src/components/Turnos/ModalPortal.jsx
@@ -7,7 +7,7 @@ const ModalPortal = ({ isOpen, onClose, title, children, actions }) => {
 
     return ReactDOM.createPortal(
         <div className="modal-overlay" onClick={onClose}>
-            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <div className="modal-content1" onClick={(e) => e.stopPropagation()}>
                 {title && <h2 className="modal-title">{title}</h2>}
                 {children && <div className="modal-body">{children}</div>}
                 {actions && <div className="modal-actions">{actions}</div>}
